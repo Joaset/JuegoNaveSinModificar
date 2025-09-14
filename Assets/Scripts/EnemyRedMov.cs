eed0747b4b0a683a00d8871675cf4078285d5d07
maurochoque <mauro91choque@gmail.com>
2025-09-13 21:54:00 -0300

    nivel 3 completo

--- Assets/Scripts/EnemyRedMov.cs
+++ Assets/Scripts/EnemyRedMov.cs
@@ -2,16 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BulletMovRedEnemy : MonoBehaviour
+public class EnemyRedMov : MonoBehaviour
 {
+    //[SerializeField] private Vector3 speed = new Vector3(2f, 0f, 0f);
+    // valor por defecto, pero se puede cambiar en Inspector
     [SerializeField] private float speed = 2.5f;
-
     void Update()
     {
+        //transform.position -= speed * Time.deltaTime;
         transform.position += Vector3.left * speed * Time.deltaTime;
-        if (transform.position.x <= -10f) 
+
+        if (transform.position.x <= -10f)
         {
             Destroy(gameObject);
         }
     }
 }
+
