8be67eb20ceeced7691e2ce4a33bda4199965179
Joaset <jtf1wft@gmail.com>
2025-11-10 23:21:51 -0300

    nuevos sprites
    se agrego nuevos sprites y se modifico la direccion de las naves en el menu principal

--- Assets/Scripts/MovMenuNave.cs
+++ Assets/Scripts/MovMenuNave.cs
@@ -8,14 +8,14 @@ public class MovMenuNave : MonoBehaviour
 
     void Start()
     {
-        speed = new Vector3(1f, 0f, 0f); // velocidad en unidades/segundo
+        speed = new Vector3(0f, 1f, 0f); // velocidad en unidades/segundo
     }
     void Update()
     {
       transform.position += speed*Time.deltaTime;
-        if (transform.position.x >= (12))
+        if (transform.position.y >= (6))
         {
-            transform.position = new Vector3(-9.39f, transform.position.y, transform.position.z);
+            transform.position = new Vector3(transform.position.x, -6, transform.position.z);
         }
     }
 }
